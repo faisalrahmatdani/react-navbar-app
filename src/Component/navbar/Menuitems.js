49a5b6962ddd5b5c1078d7a164027c06e2a16c9c
faisalrahmatdani <faisalrahmatdani07@gmail.com>
2021-10-21 23:28:38 +0700

    first commit

---
+++ src/Component/navbar/Menuitems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    tittle: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    tittle: "Services",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    tittle: "Product",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    tittle: "Contact Us",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    tittle: "Sign Up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
